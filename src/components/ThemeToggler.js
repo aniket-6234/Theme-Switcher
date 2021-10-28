fd7df749178ac22bcbf1c05d15ff744715379e7a
aniket-6234 <aniketashok1999@gmail.com>
2021-10-28 07:28:21 +0530

    logic (dark : light)

---
+++ src/components/ThemeToggler.js
@@ -0,0 +1,21 @@
+import React, {useContext} from 'react'
+import ThemeContext from '../Context/ThemeContext'
+
+
+const ThemeToggler = () => {
+   const [themeMode, setThemeMode] = useContext(ThemeContext)
+
+   return (
+     <div onClick={ () => {
+       setThemeMode(themeMode === "light" ? "dark" : "light")
+     }}>
+       <p style={{
+         cursor:"pointer",
+       }}>
+         {themeMode === "light" ? "Turn Off" : "Lights On"}
+       </p>
+     </div>
+   )
+}
+
+export default ThemeToggler;
